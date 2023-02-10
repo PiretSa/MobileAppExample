754eb091e2cd0ab8d29c2b64360dc8a4d2c064a7
Student <student@LT-27627.khk.local>
2023-02-10 14:18:25 +0200

    03-authentication-screens

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,15 @@
-import react from "react";
+import React from "react";
 import {Pressable, Text} from "react-native";
 
 import {styles} from './styles';
 
-const Button = ({title, onPress}) => {
+const Button = ({title, onPress, style}) => {
     const handlePress = () => {
         console.log('button is clicked')
     }
 
     return (
-        <Pressable hitSlop={20} onPress={handlePress} style={styles.container}>
+        <Pressable hitSlop={20} onPress={handlePress} style={[styles.container, style]}>
             <Text style={styles.title}>{title}</Text>
         </Pressable>
     )

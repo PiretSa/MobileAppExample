ecc156a8c07c2cdd9648644b9d167704c66abc99
Student <student@LT-27627.khk.local>
2023-02-17 13:04:49 +0200

    Create favorites screen

--- src/screens/app/Favorites/index.js
+++ src/screens/app/Favorites/index.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native";
+import { View, FlatList } from "react-native";
 import { styles } from "./styles";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { products } from "../../../data/products";
+import FavoriteItem from "../../../components/FavoriteItem";
+import Header from "../../../components/Header";
 
 const Favorites = () => {
+    const renderItem = ({item}) => {
+        return (
+            <FavoriteItem
+            {...item}
+            />)
+    }
+
     return (
         <SafeAreaView>
             <View style={styles.container}>
-                <Text>Favorites</Text>
+                <Header title="Favorites"/>
+                <FlatList data={products} renderItem={renderItem}
+                keyExtractor={(item) => String(item.id)}/>
             </View>
         </SafeAreaView>
-    )
+    )    
 }
 export default React.memo(Favorites)
